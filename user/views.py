1171041acef70f9961786a1b8d29fafa1f620eef
Oybekt <tursunboyevjahongir187@gmail.com>
2024-02-22 17:23:34 +0500

    e2wd

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def sign_up(request):
+    return render(request, 'regitration/signup.html')
+
+
+def sign_in(request):
+    return render(request, 'regitration/signin.html')
